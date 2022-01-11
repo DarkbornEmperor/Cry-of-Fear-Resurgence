551907ba3d62baa067dcaa4c24082607bd1f6ad7
DarkbornEmperor <darkandrai1987@gmail.com>
2022-01-11 08:55:21 -0500

    Forgot to update some things

--- lua/entities/npc_vj_cofrc_dasubergator/init.lua
+++ lua/entities/npc_vj_cofrc_dasubergator/init.lua
@@ -50,6 +50,8 @@ ENT.SoundTbl_Impact = {
 "vj_cofr/fx/flesh6.wav",
 "vj_cofr/fx/flesh7.wav"
 }
+-- Custom
+ENT.DropCoFAmmo = {"weapon_cof_syringe","ent_cof_glock_ammo","ent_cof_g43_ammo","ent_cof_m16_ammo","ent_cof_p345_ammo","ent_cof_revolver_ammo","ent_cof_rifle_ammo","ent_cof_shotgun_ammo","ent_cof_tmp_ammo","ent_cof_vp70_ammo"}
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:Gator_CustomOnInitialize()
     self.SoundTbl_Alert = {
@@ -89,8 +91,30 @@ function ENT:CustomOnAlert()
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 function ENT:CustomOnInitialKilled(dmginfo, hitgroup)
+    self:SetSolid(SOLID_NONE)
     self:AddFlags(FL_NOTARGET) -- So normal NPCs can stop shooting at the corpse
-end  
+       if GetConVarNumber("VJ_COFR_DropAmmo") == 0 or !file.Exists("lua/weapons/weapon_cof_glock.lua","GAME") then return end
+	   local pickedAmmoType = VJ_PICK(self.DropCoFAmmo)
+	   if pickedAmmoType != false then	   
+	   local AmmoDrop = ents.Create(pickedAmmoType)	   
+	   AmmoDrop:SetPos(self:GetPos() + self:OBBCenter())
+	   AmmoDrop:SetLocalAngles(self:GetAngles())	   
+	   //AmmoDrop:SetParent(self)
+	   AmmoDrop:Spawn()
+	   AmmoDrop:Activate()
+	   //self:DeleteOnRemove(AmmoDrop)
+	   
+		local phys = AmmoDrop:GetPhysicsObject()
+			if IsValid(phys) then
+				local dmgForce = (self.SavedDmgInfo.force / 40)
+				if self.DeathAnimationCodeRan then
+					dmgForce = self:GetMoveVelocity() == defPos
+end
+				phys:SetMass(1)
+				phys:ApplyForceCenter(dmgForce)
+		end		
+	end		
+end
 /*-----------------------------------------------
 	*** Copyright (c) 2012-2022 by DrVrej, All rights reserved. ***
 	No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
