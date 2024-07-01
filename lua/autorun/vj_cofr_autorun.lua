8cab80acfeaf55ca213a1b096d909a4d40565a44
Darkborn <darkandrai1987@gmail.com>
2024-07-01 16:53:39 -0400

    Update vj_cofr_autorun.lua

--- lua/autorun/vj_cofr_autorun.lua
+++ lua/autorun/vj_cofr_autorun.lua
@@ -4,7 +4,7 @@ local AddonType = "NPC"
 -------------------------------------------------------
 local VJExists = file.Exists("lua/autorun/vj_base_autorun.lua","GAME")
 if VJExists == true then
-	include('autorun/vj_controls.lua')
+	include("autorun/vj_controls.lua")
 
 	-- Cry of Fear --
 	local spawnCategory = "CoF Resurgence: CoF" 
