b46227896101d237851c16ddcfcc85a96076d477
Darkborn <darkandrai1987@gmail.com>
2025-11-05 11:14:18 -0500

    Accurate Twitcher stats for other variants

--- lua/entities/npc_vj_cofraom_twitcher2/init.lua
+++ lua/entities/npc_vj_cofraom_twitcher2/init.lua
@@ -6,3 +6,10 @@ include("shared.lua")
     No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
     without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
 -----------------------------------------------*/
+ENT.StartHealth = 190
+ENT.MeleeAttackDamage = 25
+/*-----------------------------------------------
+    *** Copyright (c) 2012-2025 by DrVrej, All rights reserved. ***
+    No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
+    without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
+-----------------------------------------------*/
